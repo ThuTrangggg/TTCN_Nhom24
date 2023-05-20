8ff6227d7db656e5cc7f04cac84d370738a07455
hakusan18 <comchaya@gmail.com>
2023-05-20 12:17:55 +0700

    thuy

--- sidebar.php
+++ sidebar.php
@@ -1,25 +1,6 @@
 <?php include 'connect.php' ?>
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title> admin- Dashboard</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
 <body id="page-top">
 
     <!-- Page Wrapper -->
@@ -63,11 +44,11 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Dự án </a>
-                        <a class="collapse-item" href="cards.html"> loại dự án </a>
-                        <a class="collapse-item" href="buttons.html">Ý tưởng </a>
-                        <a class="collapse-item" href="buttons.html">Kế hoạch quảng cáo </a>
-                        <a class="collapse-item" href="buttons.html"> Báo cáo </a>
+                        <a class="collapse-item" href="buttons.php">Dự án </a>
+                        <a class="collapse-item" href="cards.php"> loại dự án </a>
+                        <a class="collapse-item" href="ytuong.php">Ý tưởng </a>
+                        <a class="collapse-item" href="buttons.php">Kế hoạch quảng cáo </a>
+                        <a class="collapse-item" href="buttons.php"> Báo cáo </a>
 
 
                     </div>
@@ -106,27 +87,27 @@
                 <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Login Screens:</h6>
-                        <a class="collapse-item" href="login.html">Login</a>
-                        <a class="collapse-item" href="register.html">Register</a>
-                        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+                        <a class="collapse-item" href="login.php">Login</a>
+                        <a class="collapse-item" href="register.php">Register</a>
+                        <a class="collapse-item" href="forgot-password.php">Forgot Password</a>
                         <div class="collapse-divider"></div>
                         <h6 class="collapse-header">Other Pages:</h6>
-                        <a class="collapse-item" href="404.html">404 Page</a>
-                        <a class="collapse-item" href="blank.html">Blank Page</a>
+                        <a class="collapse-item" href="404.php">404 Page</a>
+                        <a class="collapse-item" href="blank.php">Blank Page</a>
                     </div>
                 </div>
             </li>
 
             <!-- Nav Item - Charts -->
             <li class="nav-item">
-                <a class="nav-link" href="charts.html">
+                <a class="nav-link" href="charts.php">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Charts</span></a>
             </li>
 
             <!-- Nav Item - Tables -->
             <li class="nav-item">
-                <a class="nav-link" href="tables.html">
+                <a class="nav-link" href="tables.php">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Tables</span></a>
             </li>
@@ -148,6 +129,7 @@
 
         </ul>
         <!-- End of Sidebar -->
+    </div>
 
 </body>
 
