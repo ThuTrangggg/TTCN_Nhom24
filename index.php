390f1d27054a59332c3c1b16d3f14c8336fc95f0
duongthuy123 <duongthuy09072002@gmail.com>
2023-05-21 15:46:46 +0700

    tt

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
+                        <!-- <h6 class="collapse-header">Custom Components:</h6> -->
                         <a class="collapse-item" href="buttons.html">Dự án </a>
                         <a class="collapse-item" href="cards.html"> loại dự án </a>
                         <a class="collapse-item" href="buttons.html">Ý tưởng  </a>
